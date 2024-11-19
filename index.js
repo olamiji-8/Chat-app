bdcb49e56d7dc4f410929c5747e522cfd5164815
olamiji-8 <olamijisotomi@gmail.com>
2024-11-20 00:50:39 +0100

    Adding a web socket

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-
-const express = require('express');
-const cors = require('cors');
-
-const dbconnect = require('./config/dbconnect');
-
-const requestLogger = require('./middlewares/requestLogger');
-
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const dotenv = require("dotenv").config();
 
 const app = express();
-app.use(cors());
+const PORT = process.env.PORT || 4000; 
 
-const dotenv = require('dotenv').config();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5000",
+        methods: ["GET", "POST"]
+    }
+});
 
-const PORT = process.env.PORT || 4000;
-dbconnect();
+io.on("connection", (socket) => {
+    console.log("A user connected");
+    socket.on("message", (msg) => {
+        io.emit("message", msg); 
+    });
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+});
 
-app.use(express.json());
-app.use(requestLogger)
-
-
-
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
