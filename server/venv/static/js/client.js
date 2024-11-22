fd0895a7991ee9736246b5167ef6340791bccdb3
olamiji-8 <olamijisotomi@gmail.com>
2024-11-22 11:08:13 +0100

    update the code by adding the bot image

--- server/venv/static/js/client.js
+++ server/venv/static/js/client.js
@@ -1,31 +1,47 @@
 const chatBody = document.getElementById("chat-body");
 const chatInput = document.getElementById("chat-input");
 const sendBtn = document.getElementById("send-btn");
+
 const socket = io("http://localhost:5000");
+
+const botAvatar = "/static/bot-avatar.png"; 
+const userAvatar = "/static/user-avatar.png"; 
+
 socket.on("message", (response) => {
-    addMessage(response, "bot");
-  });
-  sendBtn.addEventListener("click", sendMessage);
-  
-  chatInput.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      sendMessage();
-    }
-  });
-  function sendMessage() {
-    const message = chatInput.value.trim();
-    if (message) {
-      addMessage(message, "user");
-      socket.emit("message", message); 
-      chatInput.value = ""; 
-    }
+  addMessage(response, "bot");
+});
+
+sendBtn.addEventListener("click", () => {
+  sendMessage();
+});
+
+chatInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
   }
-  function addMessage(text, sender) {
-    const messageElement = document.createElement("div");
-    messageElement.className = `message ${sender}`;
-    messageElement.textContent = text;
-  
-    chatBody.appendChild(messageElement);
-    chatBody.scrollTop = chatBody.scrollHeight;
+});
+
+function sendMessage() {
+  const message = chatInput.value.trim();
+  if (message) {
+    addMessage(message, "user");
+    socket.emit("message", message); 
+    chatInput.value = ""; 
   }
+}
+
+function addMessage(text, sender) {
+  const messageContainer = document.createElement("div");
+  messageContainer.className = `message-container message ${sender}`;
+  const avatar = document.createElement("img");
+  avatar.className = "avatar";
+  avatar.src = sender === "bot" ? botAvatar : userAvatar;
+  const textBox = document.createElement("div");
+  textBox.className = "text-box";
+  textBox.textContent = text;
+  messageContainer.appendChild(avatar);
+  messageContainer.appendChild(textBox);
+
+  chatBody.appendChild(messageContainer);
+  chatBody.scrollTop = chatBody.scrollHeight;
+}
