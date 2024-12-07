bf73c382a4e25b855fb746bd4f289ebd5978a4de
olamiji-8 <olamijisotomi@gmail.com>
2024-12-07 14:05:09 +0100

    u

--- middlewares/requestLogger.js
+++ middlewares/requestLogger.js
@@ -1,6 +1,22 @@
 function requestLogger(req, res, next) {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-    next();
-  }
+  const { method, url, headers, query, body } = req;
+
+  console.log(`[${new Date().toISOString()}] ${method} ${url}`);
   
-  module.exports = requestLogger;
+  // Log request headers
+  console.log("Headers:", JSON.stringify(headers, null, 2));
+  
+  // Log query parameters if they exist
+  if (Object.keys(query).length > 0) {
+    console.log("Query Parameters:", JSON.stringify(query, null, 2));
+  }
+
+  // Log request body safely
+  if (["POST", "PUT", "PATCH"].includes(method) && Object.keys(body).length > 0) {
+    console.log("Body:", JSON.stringify(body, null, 2));
+  }
+
+  next();
+}
+
+module.exports = requestLogger;
